eb91085d427d7f1bc126604c6531f2b6e6a4676c
nakamurapomeo <nakamurapomeo@gmail.com>
2026-01-11 10:50:38 +0900

    chore: enable relative paths for GH Pages support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+    base: './', // GitHub Pages用に相対パスに変更
     plugins: [
         react(),
         VitePWA({
@@ -16,22 +17,22 @@ export default defineConfig({
                 background_color: '#0f172a',
                 display: 'fullscreen',
                 orientation: 'portrait',
-                start_url: '/',
-                scope: '/',
-                id: '/',
+                start_url: './',
+                scope: './',
+                id: './',
                 icons: [
                     {
-                        src: '/icon-192.png',
+                        src: 'icon-192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: '/icon-512.png',
+                        src: 'icon-512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     },
                     {
-                        src: '/icon-512.png',
+                        src: 'icon-512.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'maskable'
