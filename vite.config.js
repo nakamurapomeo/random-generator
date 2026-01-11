393bf2aed73cb3a7944686d34cd7b12809c6a227
nakamurapomeo <nakamurapomeo@gmail.com>
2026-01-11 09:03:01 +0900

    feat: add PWA support for Android app installation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,62 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'icon-192.png', 'icon-512.png'],
+            manifest: {
+                name: 'ランダムジェネレーター',
+                short_name: 'ランダム',
+                description: '項目からランダムに選択するジェネレーター',
+                theme_color: '#7c3aed',
+                background_color: '#0f172a',
+                display: 'standalone',
+                orientation: 'portrait',
+                start_url: './',
+                scope: './',
+                icons: [
+                    {
+                        src: 'icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    }
+                ]
+            }
+        })
+    ],
     base: './'
 })
